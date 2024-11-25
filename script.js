8133c181233bdd78112f48e1678bcd3f5526d9c2
Alex-Zhitmarev <41939288+Alex-Zhitmarev@users.noreply.github.com>
2024-11-25 23:13:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ window.addEventListener('load', () => {
        spread: 160,              // Wider spread
        origin: { y: 0.2 },       // Start higher
        scalar: 1.5,              // Bigger particles
-       colors: ['#ff0000', '#ffffff', '#000000'],
+       colors: ['#d62222', '#ffffff', '#000000'],
        startVelocity: 50,        // Faster initial speed
        gravity: 1.5,             // Slower fall
        ticks: 150                // Longer duration
